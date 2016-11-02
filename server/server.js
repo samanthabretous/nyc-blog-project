ac1d97c70ab96cc569c085e6ab6606a2c2c9febe
Samantha Bretous <sam@samanthabretous.com>
2016-11-02 14:52:48 -0400

    loaded in bundle file

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const postModel = require('./blogPost/blogPost-model')
 const BlogPost = mongoose.model('BlogPost');
 
@@ -18,18 +19,12 @@ const models = require('./index').models
 const routes = require('./index').routes
 
 db.on('open', function() {
+  app.use(bodyParser.urlencoded({extended:false}))
+  app.use(express.static('client/js/bundle'))
   app.use('/blogpost', routes.blogPost)
   app.use('*', routes.home)
   app.listen(5555, function() {
     console.log('Listening on port 5555');
-
-    BlogPost.create({
-       title: 'making a title'
-    }, function (err, data) {
-      if(err) console.log('Error with database');
-      else console.log('Post created')
-    })
-  
   })
 })
 
